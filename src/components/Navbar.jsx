920e2d9ab3a55975c753326aa16ba8ec0845b19f
jibrillarbi <158165686+jibrillarbi@users.noreply.github.com>
2025-10-02 22:56:50 +0100

    basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+
+      <div className='flex fixed w-full top-0 bg-gray-700 p-5 justify-between'>
+
+        <div className='text-lg font-extrabold'>
+          <span className='text-green-600'>Fin</span>Trackr
+        </div>
+        <div className=' flex gap-5'>
+          <button className='btn btn-m'>Log In</button>
+          <button className='btn btn-m'>Sign Up</button>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
